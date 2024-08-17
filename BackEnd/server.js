d90e6187ce335915d2e54a358355c7e2852d7eb5
Mazen Afifi <mazen.afifi@flairstech.com>
2024-08-17 10:58:14 +0300

    last changes before the deploy

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import path from "path";
-import cors from 'cors'
+
 
 
 dotenv.config()
@@ -28,12 +28,6 @@ const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
 
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://thoughts-app-vercel.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.  // Add other headers if needed.
-}))
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true }))
