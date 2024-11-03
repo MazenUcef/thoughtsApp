8b56c8d1a894673f30bd0a7acf5084bd31713fdc
Mazen Afifi <mazen.afifi@flairstech.com>
2024-11-03 21:03:17 +0200

    d

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,47 +9,50 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import path from "path";
 
-dotenv.config();
+
+
+dotenv.config()
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+    // secure: true,
+    // url: (public_id) => `https://res.cloudinary.com/dawid-kowalski/${public_id}`,
+    // format: "jpg",
+    // quality: 100
+})
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/notifications", notificationsRoutes);
 
+app.use(express.json({ limit: "5mb" }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", usersRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/notifications", notificationsRoutes)
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "FrontEnd/dist")));
-
-    // Serve the frontend index.html for any unknown routes
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'FrontEnd', 'dist', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname, "/FrontEnd/dist")))
 }
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'FrontEnd', 'dist', 'index.html'));
+});
+
+
+// console.log(process.env.MONGO_URL);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    mongoose.set("strictQuery", false);
-    mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.error("Error connecting to MongoDB:", err);
-    });
-});
+    mongoose.set("strictQuery", false)
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => { console.log("Connected to MongoDB") })
+        .catch((err) => console.log(err));
+})
